ca1b707b0650adee925a6fac47ef170d90e4a545
Zander <zanderchim78@gmail.com>
2026-04-02 23:03:20 +0100

    refactor: remove unused job sharing and saving buttons from JobDetailPage

--- frontend/src/pages/job/JobDetailPage.tsx
+++ frontend/src/pages/job/JobDetailPage.tsx
@@ -166,26 +166,6 @@ export default function JobDetailPage() {
             <button className="btn btn-primary btn-lg">
               Apply Now
             </button>
-            <button className="btn btn-secondary">
-              Save Job
-            </button>
-          </div>
-
-          <div className="job-share-card">
-            <h4>Share this job</h4>
-            <div className="share-buttons">
-              <button className="share-btn" title="Copy link">
-                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                  <rect x="9" y="9" width="13" height="13" rx="2" ry="2"/>
-                  <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/>
-                </svg>
-              </button>
-              <button className="share-btn" title="Share on LinkedIn">
-                <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
-                </svg>
-              </button>
-            </div>
           </div>
         </aside>
       </div>
